f9e50a426187f96d9d9c1218bb698af4b2971b97
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-02-11 10:08:17 -0800

    first commit

---
+++ MidtermExam.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main() {
+	// problem 1 test
+	harmonicMeans();
+	cout << endl;
+
+	// problem 2 tests
+	string testString1 = "Hello Ernie!";
+	cout << testString1 << " with cap Es: ";
+	capEs(testString1);
+	cout << testString1 << endl;
+
+	testString1 = "Eeeee Eee Eeee";
+	cout << testString1 << " with cap Es: ";
+	capEs(testString1);
+	cout << testString1 << endl << endl;
+
+	// problem 3 tests
+	int n = 101100;
+	cout << n << " in binary: " << binaryToDecimal(n) << endl;
+	n = 1000;
+	cout << n << " in binary: " << binaryToDecimal(n) << endl << endl;
+
+
+	// problem 4 tests
+	vector<int> v{ 1, 2, 2, 3, 2, 2, 3 };
+	cout << "before removeConsecutiveDuplicates: ";
+	for (int i = 0; i < v.size(); ++i) {
+		cout << v[i] << ' ';
+	}
+	removeConsecutiveDuplicates(v);
+	cout << endl << "after removeConsecutiveDuplicates:  ";
+	for (int i = 0; i < v.size(); ++i) {
+		cout << v[i] << ' ';
+	} 
+	cout << endl;
+
+	vector<int> w{ 11, 11, 11, 44, 44, 0, 29, 33, 33, 33, 33, 33 };
+	cout << "before removeConsecutiveDuplicates: ";
+	for (int i = 0; i < w.size(); ++i) {
+		cout << w[i] << ' ';
+	}
+	removeConsecutiveDuplicates(w);
+	cout << endl << "after removeConsecutiveDuplicates:  ";
+	for (int i = 0; i < w.size(); ++i) {
+		cout << w[i] << ' ';
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}

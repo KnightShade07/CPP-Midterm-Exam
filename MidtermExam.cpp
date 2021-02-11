ab4c0e630ff30919abca9aca7d706b13430f20d4
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-02-11 13:43:39 -0800

    Issue#1 harmonic means (#5)
    * Added Basic TODO Comments
    
    * Finished Function Prototypes
    
    * Finished Function Definitions
    
    * Added comment about constraints for problem 3
    
    * Removed space
    
    * Made initial function for harmonicMeans
    
    * Added problem steps
    
    * Redid function to be closer to sample output.
    
    * Added problem 1 description
    
    * Initial number input is working
    
    * Added Spacing
    
    * Program now accepts multiple inputs, ends with a 0
    
    * Added Formatting to output statement

--- MidtermExam.cpp
+++ MidtermExam.cpp
@@ -2,8 +2,12 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
-
+//TODO: If needed, extra libraries go here
 using namespace std;
+
+//TODO: Function Prototypes (calls) go here.
+
+int harmonicMeans();
 
 
 int main() {
@@ -56,4 +60,85 @@ int main() {
 
 	system("pause");
 	return 0;
+}
+
+//TODO: Function Definitions go here.
+
+/**
+ * 
+    Write a function harmonicMeans that repeatedly asks the user for two int values until at least one of them is 0. For each pair, 
+	the function should calculate and display the harmonic mean of the numbers. 
+	The harmonic mean of the numbers is the inverse of the average of the inverses. The harmonic mean of x and y can be calculated as:
+    harmonic_mean = 2.0 * x * y / (x + y);
+
+ 
+
+Sample output:
+
+Enter two integer values: 2 4
+
+The harmonic mean of 2 and 4 is 2.66667
+
+Enter two integer values: 2 6
+
+The harmonic mean of 2 and 6 is 3
+
+Enter two integer values: 2 9
+
+The harmonic mean of 2 and 9 is 3.27273
+
+Enter two integer values: 2 0
+ **/
+
+int harmonicMeans() {
+	int x;
+	int y;
+	float harmMeansEquation;
+	bool runProgram =  true;	
+
+	while (runProgram)
+	{
+		//Step 4:  Equation  & Console Output
+
+		cout << "Enter 2 integer (whole) numbers: ";
+		cin >> x;
+		cin >> y;
+
+		harmMeansEquation = 2.0 * x * y / (x + y);
+
+		if (x != 0 && y != 0)
+		{
+			cout << "The harmonic mean of " << x << " and " << y << " is " << harmMeansEquation << endl;
+		}
+		
+		if (x == 0 || y == 0)
+		{
+			runProgram = false;
+		}
+		
+
+	}
+
+	return 0;	
+
+}
+
+string capEs(string cappedWord) {
+
+}
+
+//Problem 3 Notes
+/**
+ * Do not use a string in your solution.
+ * Do not use any container such as array or vector.
+ * Also do not use any built-in base conversion functions from the system libraries.
+ * (This means stuff like std::stoi)
+ * */
+
+int binaryToDecimal(int num) {
+
+}
+
+vector<int> removeConsecutiveDuplicates(vector<int> &v){
+
 }
